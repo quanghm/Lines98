5d8a960e960c4e9a359dac2ac2e271d33d98a8ed
Quang Hoang <Quang Hoang@192.168.2.36>
2015-03-04 21:24:25 +0900

    a

--- js/script.js
+++ js/script.js
@@ -56,10 +56,14 @@ function toggleActive(object) {
 	object.toggleClass("active");
 }
 
-function isEmpty(object) {
-	return (object.data("color") == 0);
+function isEmpty(sCellID) {
+	return ($("#"+object).data("color") == 0);
 }
-
+function getNbhd(sCellID){
+	var aCord;
+	
+	
+}
 function selectCell(element) {
 	var oSelectedElement = $(element);
 	var sSelectedID = oSelectedElement.attr("id");
@@ -101,189 +105,61 @@ function selectCell(element) {
 }
 
 function findPath(sSourceCellID, sTargetCellID) {
-
-	// list of open cells
-	var aOpenCellIDs = [ sTargetCellID ];
-
-	// tracking next cell
-	var aNextCellIDs = [];
-
-	// working Cell ID
-	var sWorkingCellID = "";
-
-	// distance to target
+	// auxiliary functions
+	
+	// basic variables
+	// distances to target
 	var aDistToTarget = [];
-	for (x = 0; x < 9; x++) {
-		for (y = 0; y < 9; y++) {
-			sWorkingCellID = Cord2ID(x, y);
-			aDistToTarget[sWorkingCellID] = 20;
-			aNextCellIDs[sWorkingCellID] = "";
+	
+	// unvisited cells
+	var aUnvisitedCells =[];
+	
+	// temp variables
+	var x,y,sCurrentCell;
+	var nMinDistToTarget=0;
+	
+	// initialization
+	for (x=0;x<8;x++){
+		for (y=0;y<8;y++){
+			sCurrentCell = Cord2ID(x,y);
+			
+			if (isEmpty(sCurrentCell)){
+				aDistToTarget[sCurrentCell]=(sTargetCellID==sCurrentCell)?0:20;
+				aUnvisitedCells.push();
+				$("#"+sCurrentCell).addClass("open");
+			}
 		}
 	}
-	aDistToTarget[sTargetCellID] = 0;
-
-	function move(sSourceCellID, sTargetCellID) {
-		var sNextID = sSourceCellID;
-		var oCurrentCell;
-
-		alert(sNextID);
-
-		while (sNextID !== sTargetCellID) {
-			// move out
-			toggleActive($("#" + sNextID));
-			// get next ID
-			sNextID = aNextCellIDs[sNextID];
-			// move in
-			toggleActive($("#" + sNextID));
-		}
+	// end intialization
+	
+	while (aUnvisitedCells.length>0){
+		// find shortest distance
+		nMinDistToTarget=Math.min.apply(Math,aDistToTarget);
+		
+		// current cell ID
+		sCurrentCell=$.inArray(nMinDistToTarget,aDistToTarget);
+		
+		
 	}
-
-	// function insert ID to list of open cells
-	function insertID(sCellID) {
-		var nOpenCellCount = aOpenCellIDs.length;
-		var nKey = -0;
-
-		if (nOpenCellCount == 0) {
-			aOpenCellIDs.push(sCellID);
-		} else {
-			// check if sCellID is already open
-			nKey = $.inArray(sCellID, aOpenCellIDs);
-			if (nKey > -1) {
-				aOpenCellIDs.splice(nKey, 1);
-			}
-			nKey = -1;
-			do {
-				nKey++;
-			} while ((aDistToTarget[sCellID] > aDistToTarget[aOpenCellIDs[nKey]])
-					&& (nKey < nOpenCellCount));
-			aOpenCellIDs.splice(nKey, 0, sCellID);
-		}
-		$("#" + sCellID).addClass("open");
-	}
-
-	// while there are open cells
-	var sCurrentID = "";
-	var aCurrentCord = [];
-
-	// Previous cell info
-	var nPrevX = 0;
-	var nPrevY = 0;
-	var sPrevID = "";
-	var oPrevCell;
-
-	while (aOpenCellIDs.length > 0) {
-		sCurrentID = aOpenCellIDs[0];
-		aCurrentCord = ID2Cord(sCurrentID);
-
-		$("#" + sCurrentID).toggleClass("current");
-
-		// look at neighborhood
-		// look up
-		if (aCurrentCord[0] > 0) {
-
-			nPrevX = aCurrentCord[0]*1 - 1;
-			nPrevY = aCurrentCord[1];
-			sPrevID = Cord2ID(nPrevX, nPrevY);
-
-			oPrevCell = $("#" + sPrevID);
-			// if this is empty cell
-			if ((isEmpty(oPrevCell)) || (sPrevID == sSourceCellID)) {
-				// update distance to Target of Previous Cell
-				if (aDistToTarget[sPrevID] > aDistToTarget[sCurrentID]) {
-					aDistToTarget[sPrevID] = aDistToTarget[sCurrentID] + 1;
-					oPrevCell.html(aDistToTarget[sPrevID]);
-					aNextCellIDs[sPrevID] = sCurrentID;
-				}
-
-				// if we found the source
-				if (sPrevID == sSourceCellID) {
-					return true;
-				}
-
-				insertID(sPrevID);
-			}
-		}
-
-		// look down
-		if (aCurrentCord[0] < 8) {
-			nPrevX = aCurrentCord[0]*1 + 1;
-			nPrevY = aCurrentCord[1];
-			sPrevID = nPrevX + "_" + nPrevY;
-
-			oPrevCell = $("#" + sPrevID);
-			// if this is empty cell
-			if ((isEmpty(oPrevCell)) || (sPrevID == sSourceCellID)) {
-				// update distance to Target of Previous Cell
-				if (aDistToTarget[sPrevID] > aDistToTarget[sCurrentID]) {
-					aDistToTarget[sPrevID] = aDistToTarget[sCurrentID] + 1;
-					oPrevCell.html(aDistToTarget[sPrevID]);
-					aNextCellIDs[sPrevID] = sCurrentID;
-				}
-
-				// if we found the source
-				if (sPrevID == sSourceCellID) {
-					return true;
-				}
-
-				insertID(sPrevID);
-			}
-		}
-
-		// look left
-		if (aCurrentCord[1] > 0) {
-			nPrevX = aCurrentCord[0];
-			nPrevY = aCurrentCord[1]*1 - 1;
-			sPrevID = nPrevX + "_" + nPrevY;
-
-			oPrevCell = $("#" + sPrevID);
-			// if this is empty cell
-			if ((isEmpty(oPrevCell)) || (sPrevID == sSourceCellID)) {
-				// update distance to Target of Previous Cell
-				if (aDistToTarget[sPrevID] > aDistToTarget[sCurrentID]) {
-					aDistToTarget[sPrevID] = aDistToTarget[sCurrentID] + 1;
-					oPrevCell.html(aDistToTarget[sPrevID]);
-					aNextCellIDs[sPrevID] = sCurrentID;
-				}
-
-				// if we found the source
-				if (sPrevID == sSourceCellID) {
-					return true;
-				} else {
-					insertID(sPrevID);
-				}
-			}
-		}
-
-		// look right
-		if (aCurrentCord[1] < 8) {
-			nPrevX = aCurrentCord[0];
-			nPrevY = aCurrentCord[1]*1 + 1;
-			sPrevID = nPrevX + "_" + nPrevY;
-
-			oPrevCell = $("#" + sPrevID);
-			oPrevCell.toggleClass("working");
-			alert(sPrevID);
-			// if this is empty cell
-			if ((isEmpty(oPrevCell)) || (sPrevID == sSourceCellID)) {
-				// update distance to Target of Previous Cell
-				if (aDistToTarget[sPrevID] > aDistToTarget[sCurrentID]) {
-					aDistToTarget[sPrevID] = aDistToTarget[sCurrentID] + 1;
-					oPrevCell.html(aDistToTarget[sPrevID]);
-					aNextCellIDs[sPrevID] = sCurrentID;
-				}
-
-				// if we found the source
-				if (sPrevID == sSourceCellID) {
-					return true;
-				} else {
-					insertID(sPrevID);
-				}
-			}
-		}
-		$("#" + sCurrentID).toggleClass("current");		
-		aOpenCellIDs.splice(0, 1);
-	}
-
-	// no more open cells, cannot reach Source
-	return false;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
